3bf24d8e7456b5afd0fea91568115796700ad5a7
wwyyzz <sdn_app@163.com>
2019-04-08 08:42:30 -0700

    thymeleft

--- src/main/java/com/wj/springboot/controller/HelloController.java
+++ src/main/java/com/wj/springboot/controller/HelloController.java
@@ -5,11 +5,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Arrays;
+import java.util.Map;
+
+@Controller
 public class HelloController {
 
+    @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
         return "hello world quick!";
+    }
+
+    @RequestMapping("/success")
+    public String success(Map<String, Object> map){
+        //classpath:/templates/success.html
+        map.put("hello", "<h1>nihao!</h1>");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
+        return "success";
     }
 }
