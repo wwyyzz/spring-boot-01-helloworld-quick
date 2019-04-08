9b0147ede3acff1957009687511c94e929e0c46d
wwyyzz <sdn_app@163.com>
2019-04-08 17:56:16 +0800

    web

--- src/test/java/com/wj/springboot/SpringBoot01HelloworldQuickApplicationTests.java
+++ src/test/java/com/wj/springboot/SpringBoot01HelloworldQuickApplicationTests.java
@@ -3,6 +3,8 @@ package com.wj.springboot;
 import com.wj.springboot.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -11,6 +13,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBoot01HelloworldQuickApplicationTests {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     Person person;
@@ -27,7 +31,24 @@ public class SpringBoot01HelloworldQuickApplicationTests {
 
     @Test
     public void contextLoads() {
+
         System.out.println(person);
+
+        logger.trace("This is trace log...");
+
+        logger.debug("This is debug log...");
+
+        logger.info("This is info log...");
+
+        logger.warn("This is warn log...");
+
+        logger.error("This is error log...");
+
+
+
+
     }
+
+
 
 }
